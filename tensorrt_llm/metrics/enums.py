d93cb3839e55d8a4a6622ca25d7e33ec5cb6e236
Patrick Reiter Horn <patrick.horn@gmail.com>
2025-06-28 02:24:34 +0300

    Reimplement metrics endpoint with stats about requests Plus nebius fixes
    Merged by ltalal

--- tensorrt_llm/metrics/enums.py
+++ tensorrt_llm/metrics/enums.py
@@ -1,11 +1,16 @@
 from enum import Enum
 
 
-class MetricNames(Enum):
+class MetricNames(str, Enum):
     TTFT = "ttft"
     TPOT = "tpot"
     E2E = "e2e"
     REQUEST_QUEUE_TIME = "request_queue_time"
+    GPU_PREFIX_CACHE_HIT_RATE = "gpu_prefix_cache_hit_rate"
+    KV_CACHE_TRANSFER_TIME = "kv_cache_transfer_time"
+    SPEC_DECODE_DRAFT_ACCEPTANCE_RATE = "spec_decode_draft_acceptance_rate"
+    SPEC_DECODE_ACCEPTED_TOKENS = "spec_decode_num_accepted_tokens"
+    SPEC_DECODE_DRAFT_TOKENS = "spec_decode_num_draft_tokens"
 
 
 class RequestEventTiming(Enum):
@@ -13,3 +18,5 @@ class RequestEventTiming(Enum):
     FIRST_TOKEN_TIME = "first_token_time"  # nosec: B105
     FIRST_SCHEDULED_TIME = "first_scheduled_time"
     LAST_TOKEN_TIME = "last_token_time"  # nosec: B105
+    KV_CACHE_TRANSFER_START = "kv_cache_transfer_start"
+    KV_CACHE_TRANSFER_END = "kv_cache_transfer_end"
