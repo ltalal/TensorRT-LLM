8e5208f2a67dcd222b189df43d38e92fec41df15
Leonid Talalaev <leonid.talalaev@nebius.com>
2025-08-07 19:31:52 +0300

    Error handling: - Kill the process in case of error in the event_loop - Do not crash worker on request submit errors

--- tensorrt_llm/_torch/pyexecutor/py_executor.py
+++ tensorrt_llm/_torch/pyexecutor/py_executor.py
@@ -3,7 +3,7 @@ import datetime
 import functools
 import gc
 import os
-import pickle  # nosec B403
+import sys
 import threading
 import time
 import traceback
@@ -361,7 +361,8 @@ class PyExecutor:
         except Exception as e:
             logger.error(f"Error in event loop: {e}")
             logger.error(traceback.format_exc())
-            raise e
+            logger.critical("Exiting due to error in event loop.")
+            sys.exit(1)
         finally:
             self._executor_loop_cleanup()
 
