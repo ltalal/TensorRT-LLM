fab2800f7da4ea1feb3d9aa4f386bfad61c7d0f9
Leonid Talalaev <leonid.talalaev@gmail.com>
2026-03-04 19:18:31 +0000

    Metrics
    Signed-off-by: Leonid Talalaev <leonid.talalaev@gmail.com>
    
    Metrics: fixed labels
    
    Signed-off-by: Leonid Talalaev <leonid.talalaev@gmail.com>
    
    Metrics: added missing metrics
    
    Signed-off-by: Leonid Talalaev <leonid.talalaev@gmail.com>

--- tensorrt_llm/_torch/pyexecutor/py_executor.py
+++ tensorrt_llm/_torch/pyexecutor/py_executor.py
@@ -1,11 +1,14 @@
+import array
 import dataclasses
 import datetime
 import functools
+import json
 import os
 import sys
 import threading
 import time
 import traceback
+from collections import defaultdict
 from contextlib import contextmanager
 from enum import IntEnum
 from queue import Queue
@@ -69,6 +72,10 @@ from .scheduler import (RequestScheduler, ScheduledRequests,
                         SerializableSchedulerOutput, WaitingQueue,
                         create_waiting_queue)
 from .scheduler.adp_router import ADPRouter, DefaultADPRouter
+
+PROM_METRICS_FILENAME = '/dev/shm/prom_metrics.json'
+prom_metrics = defaultdict(float)
+prom_metrics_file = None
 
 # Environment variable to specify iteration ranges for profiling start/stop.
 # Format: "start1-stop1,start2-stop2,..." or single iterations "iter1,iter2,..."
@@ -852,6 +859,7 @@ class PyExecutor:
                 torch.cuda.cudart().cudaProfilerStop()
                 calibrator.stop()
                 enabled = False
+            last_start_time = start_time
 
             if start_time is not None and self.print_log and self.dist.rank == 0:
                 end_time = time.time()
@@ -907,6 +915,44 @@ class PyExecutor:
                 if start_event_2 is None:
                     start_event_2 = torch.cuda.Event(enable_timing=True)
                 start_event_2.record()
+
+            if last_start_time is not None and self.dist.rank == 0:
+                iter_states = self.model_engine.iter_states
+                total_running = iter_states['num_ctx_requests'] + iter_states[
+                    'num_generation_tokens'] / (1 +
+                                                self.model_engine.max_draft_len)
+                prom_metrics["num_requests_running"] = total_running
+                prom_metrics["num_requests_swapped"] = total_running - len(
+                    self.active_requests)
+                prom_metrics["iteration_tokens_total_sum"] += iter_states[
+                    'num_ctx_tokens'] + iter_states['num_generation_tokens']
+                prom_metrics["iteration_tokens_total_count"] += 1
+                prom_metrics["time_per_output_token_seconds_sum"] += (
+                    start_time - last_start_time)
+                prom_metrics["time_per_output_token_seconds_count"] += 1
+                prom_metrics["prompt_tokens_total"] += iter_states[
+                    'num_ctx_tokens']
+                prom_metrics["request_prompt_tokens_total_sum"] += iter_states[
+                    'num_ctx_tokens']
+                prom_metrics["request_prompt_tokens_total_count"] += 1
+                prom_metrics["generation_tokens_total"] += iter_states[
+                    'num_generation_tokens']
+                prom_metrics[
+                    "request_generation_tokens_total_sum"] += iter_states[
+                        'num_generation_tokens']
+                prom_metrics["request_generation_tokens_total_count"] += 1
+                global prom_metrics_file
+                try:
+                    if prom_metrics_file is None:
+                        prom_metrics_file = os.open(PROM_METRICS_FILENAME,
+                                                    os.O_RDWR | os.O_CREAT)
+                    os.pwrite(
+                        prom_metrics_file,
+                        (json.dumps(list(prom_metrics.keys())).encode('UTF-8') +
+                         b'\0' +
+                         array.array('d', prom_metrics.values()).tobytes()), 0)
+                except OSError:
+                    traceback.print_exc()
 
         try:
             yield profile_step
