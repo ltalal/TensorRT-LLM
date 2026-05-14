18b400e87b026096a4b739270422d9ea2de9ea41
Leonid Talalaev <leonid.talalaev@gmail.com>
2026-05-14 12:08:41 +0000

    Nebius devtools: updates
    Signed-off-by: Leonid Talalaev <leonid.talalaev@gmail.com>

--- nebius/devtools/trtllm_serve_llama.sh
+++ nebius/devtools/trtllm_serve_llama.sh
@@ -1,3 +1,6 @@
+export PYTHONPATH=/workspaces/tensorrt_llm:${PYTHONPATH:-}
+export TLLM_NUMA_AWARE_WORKER_AFFINITY=1
+
 exec trtllm-serve serve /home/ubuntu/scratch.trt_llm_data_ci/llama-3.1-model/Llama-3.1-8B-Instruct --host 0.0.0.0 --port 8000 --tp_size 1 \
   --served_model_name meta-llama/Meta-Llama-3.1-8B-Instruct  \
   --kv_cache_free_gpu_memory_fraction 0.8 --extra_llm_api_options cfg_tests.yaml
